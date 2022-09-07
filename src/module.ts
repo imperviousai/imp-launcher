192df3dc628c9ce30bb539225a620ede4467fb09
stewIMP <stewart@impervious.ai>
2022-09-07 15:51:19 -0400

    fixed extract race cond

--- src/module.ts
+++ src/module.ts
@@ -8,22 +8,15 @@ import stream from "stream";
 import { promisify } from "util";
 import { createWindow } from "./main";
 import os from "os";
-// import { getS3LikeProviderBaseUrl } from "builder-util-runtime";
+
 const finished = promisify(stream.finished);
 import log from "electron-log";
 import { pids } from "./main"; // an array of pids that we want to kill when browser or electron closes
 
-// TODO: Check if both the daemon and browser are installed, show installation window if either of them exist
-// const daemonPath = path.join(__dirname, "../daemon/");
-// const daemonConfigPath = path.join(__dirname, "../daemon/config/config.yml");
-// const browserPath = path.join(__dirname, "../browser/");
 
-// fs.mkdirSync(browserPath, { recursive: true });
-// fs.mkdirSync(`${daemonPath}/config/`, { recursive: true });
 
-//const homePath = `/Users/${process.env.USER}/Impervious/`;
 const user = os.userInfo().username;
-//const homePath = "/Users/" + process.env.USER + "/Impervious/"; works 100%
+
 const homePath =
   os.platform() === "darwin"
     ? `/Users/${user}`
@@ -43,16 +36,12 @@ fs.mkdirSync(`${newDaemonPath}/config/`, { recursive: true });
 
 const newDaemonConfigPath = newDaemonPath + "config/config.yml";
 
-// returns a presigned URL which can then download the file
-// api key header: x-api-key
-// https://artifacts.imp-api.net/impervious-browser/Impervious.zip
-// https://artifacts.imp-api.net/impervious-macos-arm64_darwin_arm64/impervious.zip
 
 const baseURL = "https://artifacts.imp-api.net";
 let daemonDownloadURL = `${baseURL}/impervious-daemon/Impervious`;
 let browserDownloadURL = `${baseURL}/impervious-browser/Impervious`;
 const daemonConfigDownloadURL =
-  "https://raw.githubusercontent.com/imperviousai/imp-launcher/master/config.yml";
+  "https://raw.githubusercontent.com/imperviousai/imp-launcher/enable-linux-build/config.yml";
 
 if (os.platform() === "darwin") {
   if (os.arch() === "arm64") {
@@ -154,45 +143,6 @@ export const spawnBrowser = () => {
   });
 };
 
-// will need to handle this
-// const spawnInstallWindow = () => {
-//   const mainWindow = new BrowserWindow({
-//     width: 800,
-//     height: 600,
-//     webPreferences: {
-//       disableBlinkFeatures: "Auxclick",
-//       sandbox:
-//         false /* eng-disable SANDBOX_JS_CHECK -- sandbox prevents node integration */,
-//       nodeIntegration:
-//         true /* eng-disable NODE_INTEGRATION_JS_CHECK -- need to communicate with node */,
-//       contextIsolation: false /* eng-disable CONTEXT_ISOLATION_JS_CHECK*/,
-//       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
-//     },
-//   });
-
-//   mainWindow.webContents.on("will-navigate", (event, newURL) => {
-//     /* eng-disable LIMIT_NAVIGATION_JS_CHECK -- checked */
-//     if (mainWindow.webContents.getURL() !== "http://localhost") {
-//       event.preventDefault();
-//     }
-//   });
-
-//   mainWindow.webContents.on("new-window", (event, newURL) => {
-//     /* eng-disable LIMIT_NAVIGATION_JS_CHECK -- checked */
-//     if (mainWindow.webContents.getURL() !== "http://localhost") {
-//       event.preventDefault();
-//     }
-//   });
-
-//   // and load the index.html of the app.
-//   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-
-//   // Open the DevTools if in dev.
-//   if (isDev) {
-//     mainWindow.webContents.openDevTools();
-//   }
-// };
-
 const getS3URL = async (url: string) => {
   return axios({
     method: "get",
@@ -200,47 +150,27 @@ const getS3URL = async (url: string) => {
   });
 };
 
-const downloadDaemonConfig = () => {
-  const file = fs.createWriteStream(newDaemonConfigPath);
-  return axios({
-    method: "get",
-    url: daemonConfigDownloadURL,
-    responseType: "stream",
-  })
-    .then(async (response) => {
-      response.data.pipe(file);
-      file
-        .on("finish", () => {
-          log.info("File download completed.");
-        })
-        .on("close", () => {
-          log.info("File successfully closed");
-        })
-        .on("error", (err) => {
-          log.error("ERROR: ", err);
-        });
-      return await finished(file);
-    })
-    .catch((err) => {
-      log.info(err);
-    });
-};
+
+const downloadDaemonConfig = async () => {
+  try {
+    const file = fs.createWriteStream(newDaemonConfigPath);
+    const response = await axios.get(daemonConfigDownloadURL, { responseType: "stream"} );
+    console.log(response);
+    await response.data.pipe(file);
+    console.log("Downloaded config file...");
+  } catch (err) {
+    console.error("Failed to download config file...", err.message);
+  }
+}
 
 const download = async (downloadURL: string, outputPath: string) => {
   const s3URL = await getS3URL(downloadURL);
 
-  // unable to download private assets for some weird reason, seems like the authorization
-  // header is being ignored
   const file = fs.createWriteStream(outputPath);
   return axios({
     method: "get",
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     url: s3URL.data,
     responseType: "stream",
-    // TODO: required for pulling resources from private github repo
-    // headers: {
-    //   authorization: `token ${process.env.GITHUB_PAT}`,
-    // },
   })
     .then(async (response) => {
       response.data.pipe(file);
@@ -260,31 +190,6 @@ const download = async (downloadURL: string, outputPath: string) => {
       log.info(err);
     });
 };
-
-// export const downloadDaemon = (version: string) => {
-//   return new Promise((resolve, reject) => {
-//     download(daemonDownloadURL, newDaemonPath + "impervious.zip")
-//       .then(() => {
-//         // event messaging here
-//         // extract
-//         try {
-//           extract(newDaemonPath + "impervious.zip", { dir: newDaemonPath });
-//         } catch (err) {
-//           log.error(err);
-//         }
-//         // tar
-//         //   .x({ file: outputPath, cwd: path.join(__dirname, "../daemon/") })
-//         //   .then(() => console.log("File successfully extracted"));
-//         resolve("daemon extracted");
-//         return;
-//       })
-//       .catch((err) => {
-//         // event message here
-//         log.error("Unable to download file: ", err);
-//         reject();
-//       });
-//   });
-// };
 
 export const downloadDaemon = async (version: string) => {
   try {
@@ -300,29 +205,6 @@ export const downloadDaemon = async (version: string) => {
     console.error("Daemon extraction failure...");
   }
 }
-
-// export const downloadBrowser = (version: string) => {
-//   return new Promise((resolve, reject) => {
-//     download(browserDownloadURL, newBrowserPath + "impervious-browser.zip")
-//       .then(() => {
-//         // event messaging here
-//         // extract
-//         try {
-//           extract(newBrowserPath + "impervious-browser.zip", {
-//             dir: newBrowserPath,
-//           });
-//         } catch (err) {
-//           log.info("Unable to extract: ", err);
-//         }
-//         resolve("browser extracted");
-//       })
-//       .catch((err) => {
-//         // event message here
-//         log.info("Unable to download file: ", err);
-//         reject();
-//       });
-//   });
-// };
 
 export const downloadBrowser = async (version: string) => {
   try {
